f75aaa3f4bf482f7b602be0925bdff129f79c04f
Muhibbul hasan <muhibbulhasan6@gmail.com>
2023-02-20 02:28:14 +0600

    first commit

---
+++ Components/Audiance/Audiance.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Audiance = () => {
+  return (
+    <div className="grid grid-cols-2  h-[440px]">
+      <div className="flex items-start">
+        <div className="p-16">
+          <h1>OUR FEATURES</h1>
+          <h1 className="border-b-[1px] border-[#000000]">
+            Know your audience and boost sales
+          </h1>
+        </div>
+      </div>
+      <div className="flex items-end">
+        <p className="p-16">
+          Effective emails convey the most significant substance to a client at
+          the proper time which is our marketing approaches include scheduled
+          emails.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Audiance;
